2db29cec06548717a52b91613840d0d3b95bbbc2
wujun <1477563131@qq.com>
2018-11-04 23:00:56 +0800

    查询

--- item/service/src/main/java/com/gehua/ServiceApplication.java
+++ item/service/src/main/java/com/gehua/ServiceApplication.java
@@ -3,9 +3,11 @@ package com.gehua;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.gehua.item.mapper")
 public class ServiceApplication {
 
     public static void main(String[] args) {
